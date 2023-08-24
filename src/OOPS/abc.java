18c9f452a6528ca78690a61c35c235a448f44123
Yogi Chauhan <yogichauhan7171@gmail.com>
2023-08-24 12:52:09 -0400

    code uncommented

--- src/OOPS/abc.java
+++ src/OOPS/abc.java
@@ -8,7 +8,7 @@ public class abc {
 	public static void main(String[] args) {
 	 String str1 = "test";
 	 String str2 = "one";
-	// String str3= "two";
+	 String str3= "two";
 	 String str4=str1.concat(str2);
 	 System.out.println(str4);
 	 int ab[]= {1,2,3,4,5};
