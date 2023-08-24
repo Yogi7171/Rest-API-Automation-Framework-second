e0942a8abe3b3a4d9874b5b07f0873c86d6fd0b7
Yogi Chauhan <yogichauhan7171@gmail.com>
2023-08-24 10:53:28 -0400

    added string  str3=two

--- src/OOPS/abc.java
+++ src/OOPS/abc.java
@@ -8,7 +8,8 @@ public class abc {
 	public static void main(String[] args) {
 	 String str1 = "test";
 	 String str2 = "one";
-	 String str3=str1.concat(str2);
+	 String str3= "two";
+	 String str4=str1.concat(str2);
 	 System.out.println(str3);
 	 int ab[]= {1,2,3,4,5};
 	 
